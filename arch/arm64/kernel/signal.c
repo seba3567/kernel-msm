ef4bf8cb16cebd4cfc9b713cdfe420677c6ec393
seba3567 <sebastian26481@gmail.com>
2020-09-12 01:53:30 -0300

    somes disable vdso for now

--- arch/arm64/kernel/signal.c
+++ arch/arm64/kernel/signal.c
@@ -39,7 +39,6 @@
 #include <asm/fpsimd.h>
 #include <asm/ptrace.h>
 #include <asm/signal32.h>
-#include <asm/vdso.h>
 
 /*
  * Do a signal return; undo the signal stack. These are aligned to 128-bit.
@@ -583,8 +582,6 @@ static void setup_return(struct pt_regs *regs, struct k_sigaction *ka,
 	if (ka->sa.sa_flags & SA_RESTORER)
 		sigtramp = ka->sa.sa_restorer;
 	else
-		sigtramp = VDSO_SYMBOL(current->mm->context.vdso, sigtramp);
-
 	regs->regs[30] = (unsigned long)sigtramp;
 }
 
